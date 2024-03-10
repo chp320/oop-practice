f4d7fd788e12d6193dffe8b77dc76100937283ff
chp320 <chp320@naver.com>
2024-03-10 21:45:39 +0900

    Stream<Arguments> 를 활용한 테스트 코드 리팩토링

--- src/test/java/org/example2/CalculatorTest.java
+++ src/test/java/org/example2/CalculatorTest.java
@@ -2,8 +2,14 @@ package org.example2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * [요구사항]
@@ -18,18 +24,20 @@ public class CalculatorTest {
     // 피연산자 + 피연산자 ------------> Calculater
     //     결과        <------------
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculate(1, "+", 2);
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")   // 지정된 항목을 소스로 사용할꺼임
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculateResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subtractionTest() {
-        int result = Calculator.calculate(1, "-", 2);
-
-        assertThat(result).isEqualTo(-1);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
 }
