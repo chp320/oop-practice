b5f0f3534d0b693b8f32da9542460577f1704071
chp320 <chp320@naver.com>
2024-04-13 08:28:31 +0900

    메뉴 주문 테스트 코드 작성

--- src/test/java/org/example4/MenuTest.java
+++ src/test/java/org/example4/MenuTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class MenuTest {
 
@@ -19,5 +20,15 @@ public class MenuTest {
 
         // 객체 비교는 equals and hashCode() 추가!!
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    @Test
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+
+        assertThatCode(() -> menu.choose("통닭"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다.");
     }
 }
